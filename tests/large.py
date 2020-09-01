6233c2796fc837454dae3fd0a0eb3adbf7ec8ad8
Marco Devillers <marco.devillers@gmail.com>
2020-09-01 16:57:05 +0200

    update to new python/egel

--- tests/large.py
+++ tests/large.py
@@ -1,22 +1,22 @@
 
 def genmod(n):
-    print "namespace Test%d"% n, " ("
-    print "     data true, false"
-    print ""
-    print "     namespace F.G ("
-    print "         def and ="
-    print "             [ true, true -> true | _ , _ -> false ]"
-    print "     )"
-    print ""
-    print "     namespace I.J ("
-    print "         def or ="
-    print "             [ false, false -> true | _ , _ -> true ]"
-    print "     )"
-    print ""
-    print "     def not ="
-    print "         [ false -> true | _ -> false ]"
-    print ""
-    print ")"
+    print("namespace Test%d" % n, " (")
+    print("     data true, false")
+    print("")
+    print("     namespace F:G (")
+    print("         def and =")
+    print("             [ true true -> true | _ _ -> false ]")
+    print("     )")
+    print("")
+    print("     namespace I:J (")
+    print("         def or =")
+    print("             [ false false -> true | _ _ -> true ]")
+    print("     )")
+    print("")
+    print("     def not =")
+    print("         [ false -> true | _ -> false ]")
+    print("")
+    print(")")
 
 for i in range(0, 2000):
     genmod(i)
