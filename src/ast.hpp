e1c40565b202e037cd85be0dda36a98958201fbd
Marco Devillers <marco.devillers@gmail.com>
2022-06-28 00:46:02 +0200

    bored

--- src/ast.hpp
+++ src/ast.hpp
@@ -2391,59 +2391,59 @@ int Ast::compare_tag(ast_tag_t t, const AstPtr &a0, const AstPtr &a1) {
         }
         // literals
         case AST_EXPR_INTEGER: {
-            AST_EXPR_INTEGER_SPLIT(a0, p0, t0);
-            AST_EXPR_INTEGER_SPLIT(a1, p1, t1);
+            auto [p0, t0] = AstExprInteger::split(a0);
+            auto [p1, t1] = AstExprInteger::split(a1);
             return compare_text(t0, t1);
             break;
         }
         case AST_EXPR_HEXINTEGER: {
-            AST_EXPR_HEXINTEGER_SPLIT(a0, p0, t0);
-            AST_EXPR_HEXINTEGER_SPLIT(a1, p1, t1);
+            auto [p0, t0] = AstExprHexInteger::split(a0);
+            auto [p1, t1] = AstExprHexInteger::split(a1);
             return compare_text(t0, t1);
             break;
         }
         case AST_EXPR_FLOAT: {
-            AST_EXPR_FLOAT_SPLIT(a0, p0, t0);
-            AST_EXPR_FLOAT_SPLIT(a1, p1, t1);
+            auto [p0, t0] = AstExprFloat::split(a0);
+            auto [p1, t1] = AstExprFloat::split(a1);
             return compare_text(t0, t1);
             break;
         }
         case AST_EXPR_CHARACTER: {
-            AST_EXPR_CHARACTER_SPLIT(a0, p0, t0);
-            AST_EXPR_CHARACTER_SPLIT(a1, p1, t1);
+            auto [p0, t0] = AstExprCharacter::split(a0);
+            auto [p1, t1] = AstExprCharacter::split(a1);
             return compare_text(t0, t1);
             break;
         }
         case AST_EXPR_TEXT: {
-            AST_EXPR_TEXT_SPLIT(a0, p0, t0);
-            AST_EXPR_TEXT_SPLIT(a1, p1, t1);
+            auto [p0, t0] = AstExprText::split(a0);
+            auto [p1, t1] = AstExprText::split(a1);
             return compare_text(t0, t1);
             break;
         }
         // variables and constants
         case AST_EXPR_VARIABLE: {
-            AST_EXPR_VARIABLE_SPLIT(a0, p0, t0);
-            AST_EXPR_VARIABLE_SPLIT(a1, p1, t1);
+            auto [p0, t0] = AstExprVariable::split(a0);
+            auto [p1, t1] = AstExprVariable::split(a1);
             return compare_text(t0, t1);
             break;
         }
         case AST_EXPR_WILDCARD: {
-            AST_EXPR_WILDCARD_SPLIT(a0, p0, t0);
-            AST_EXPR_WILDCARD_SPLIT(a1, p1, t1);
+            auto [p0, t0] = AstExprWildcard::split(a0);
+            auto [p1, t1] = AstExprWildcard::split(a1);
             return compare_text(t0, t1);
             break;
         }
         case AST_EXPR_COMBINATOR: {
-            AST_EXPR_COMBINATOR_SPLIT(a0, p0, q0, t0);
-            AST_EXPR_COMBINATOR_SPLIT(a1, p1, q1, t1);
+            auto [p0, q0, t0] = AstExprCombinator::split(a0);
+            auto [p1, q1, t1] = AstExprCombinator::split(a1);
             c = compare_texts(q0, q1);
             if (c != 0) return c;
             return compare_text(t0, t1);
             break;
         }
         case AST_EXPR_OPERATOR: {
-            AST_EXPR_OPERATOR_SPLIT(a0, p0, q0, t0);
-            AST_EXPR_OPERATOR_SPLIT(a1, p1, q1, t1);
+            auto [p0, q0, t0] = AstExprOperator::split(a0);
+            auto [p1, q1, t1] = AstExprOperator::split(a1);
             c = compare_texts(q0, q1);
             if (c != 0) return c;
             return compare_text(t0, t1);
@@ -2451,117 +2451,119 @@ int Ast::compare_tag(ast_tag_t t, const AstPtr &a0, const AstPtr &a1) {
         }
         //  list and tuple
         case AST_EXPR_LIST: {
-            AST_EXPR_LIST_SPLIT(a0, p0, tt0, tl0);
-            AST_EXPR_LIST_SPLIT(a1, p1, tt1, tl1);
+            auto [p0, tt0, tl0] = AstExprList::split(a0);
+            auto [p1, tt1, tl1] = AstExprList::split(a1);
             c = compare_asts(tt0, tt1);
             if (c != 0) return c;
             return Ast::compare(tl0, tl1);
             break;
         }
         case AST_EXPR_TUPLE: {
-            AST_EXPR_TUPLE_SPLIT(a0, p0, tt0);
-            AST_EXPR_TUPLE_SPLIT(a1, p1, tt1);
+            auto [p0, tt0] = AstExprTuple::split(a0);
+            auto [p1, tt1] = AstExprTuple::split(a1);
             return compare_asts(tt0, tt1);
             break;
         }
         // compound statements
         case AST_EXPR_APPLICATION: {
-            AST_EXPR_APPLICATION_SPLIT(a0, p0, aa0);
-            AST_EXPR_APPLICATION_SPLIT(a1, p1, aa1);
+            auto [p0, aa0] = AstExprApplication::split(a0);
+            auto [p1, aa1] = AstExprApplication::split(a1);
             return compare_asts(aa0, aa1);
             break;
         }
         case AST_EXPR_LAMBDA: {
-            AST_EXPR_LAMBDA_SPLIT(a0, p0, m0);
-            AST_EXPR_LAMBDA_SPLIT(a1, p1, m1);
+            auto [p0, m0] = AstExprLambda::split(a0);
+            auto [p1, m1] = AstExprLambda::split(a1);
             return Ast::compare(m0, m1);
             break;
         }
         case AST_EXPR_BLOCK: {
-            AST_EXPR_BLOCK_SPLIT(a0, p0, mm0);
-            AST_EXPR_BLOCK_SPLIT(a1, p1, mm1);
+            auto [p0, mm0] = AstExprBlock::split(a0);
+            auto [p1, mm1] = AstExprBlock::split(a1);
             return compare_asts(mm0, mm1);
             break;
         }
         case AST_EXPR_MATCH: {
-            AST_EXPR_MATCH_SPLIT(a0, p0, mm0, g0, e0);
-            AST_EXPR_MATCH_SPLIT(a1, p1, mm1, g1, e1);
+            auto [p0, mm0, g0, e0] = AstExprMatch::split(a0);
+            auto [p1, mm1, g1, e1] = AstExprMatch::split(a1);
             c = compare_asts(mm0, mm1);
             if (c != 0) return c;
             return compare_ast2(g0, g1, e0, e1);
             break;
         }
         case AST_EXPR_LET: {
-            AST_EXPR_LET_SPLIT(a0, p0, l0, r0, e0);
-            AST_EXPR_LET_SPLIT(a1, p1, l1, r1, e1);
+            auto [p0, l0, r0, e0] = AstExprLet::split(a0);
+            auto [p1, l1, r1, e1] = AstExprLet::split(a1);
             c = compare_asts(l0, l1);
             if (c != 0) return c;
             return compare_ast2(r0, r1, e0, e1);
             break;
         }
         case AST_EXPR_TAG: {
-            AST_EXPR_TAG_SPLIT(a0, p0, e0, t0);
-            AST_EXPR_TAG_SPLIT(a1, p1, e1, t1);
+            auto [p0, e0, t0] = AstExprTag::split(a0);
+            auto [p1, e1, t1] = AstExprTag::split(a1);
             return compare_ast2(e0, e1, t0, t1);
             break;
         }
         case AST_EXPR_IF: {
-            AST_EXPR_IF_SPLIT(a0, p0, i0, t0, e0);
-            AST_EXPR_IF_SPLIT(a1, p1, i1, t1, e1);
+            auto [p0, i0, t0, e0] = AstExprIf::split(a0);
+            auto [p1, i1, t1, e1] = AstExprIf::split(a1);
             return compare_ast3(i0, i1, t0, t1, e0, e1);
             break;
         }
         case AST_EXPR_STATEMENT: {
-            AST_EXPR_STATEMENT_SPLIT(a0, p0, l0, r0);
-            AST_EXPR_STATEMENT_SPLIT(a1, p1, l1, r1);
+            auto [p0, l0, r0] = AstExprStatement::split(a0);
+            auto [p1, l1, r1] = AstExprStatement::split(a1);
             return compare_ast2(r0, r1, l0, l1);
             break;
         }
         case AST_EXPR_TRY: {
-            AST_EXPR_TRY_SPLIT(a0, p0, t0, c0);
-            AST_EXPR_TRY_SPLIT(a1, p1, t1, c1);
+            auto [p0, t0, c0] = AstExprTry::split(a0);
+            auto [p1, t1, c1] = AstExprTry::split(a1);
+            // XXX
             break;
         }
         case AST_EXPR_THROW: {
-            AST_EXPR_THROW_SPLIT(a0, p0, exc0);
-            AST_EXPR_THROW_SPLIT(a1, p1, exc1);
+            auto [p0, e0] = AstExprThrow::split(a0);
+            auto [p1, e1] = AstExprThrow::split(a1);
+            // XXX
             break;
         }
         // directives
         case AST_DIRECT_IMPORT: {
-            AST_DIRECT_IMPORT_SPLIT(a0, p0, n0);
-            AST_DIRECT_IMPORT_SPLIT(a1, p1, n1);
+            auto [p0, n0] = AstDirectImport::split(a0);
+            auto [p1, n1] = AstDirectImport::split(a1);
             return compare_text(n0, n1);
             break;
         }
         case AST_DIRECT_USING: {
-            AST_DIRECT_USING_SPLIT(a0, p0, pp0);
-            AST_DIRECT_USING_SPLIT(a1, p1, pp1);
+            auto [p0, pp0] = AstDirectUsing::split(a0);
+            auto [p1, pp1] = AstDirectUsing::split(a1);
             return compare_texts(pp0, pp1);
             break;
         }
         // declarations
         case AST_DECL_DATA: {
-            AST_DECL_DATA_SPLIT(a0, p0, nn0);
-            AST_DECL_DATA_SPLIT(a1, p1, nn1);
+            auto [p0, nn0] = AstDeclData::split(a0);
+            auto [p1, nn1] = AstDeclData::split(a1);
             return compare_asts(nn0, nn1);
             break;
         }
         case AST_DECL_DEFINITION: {
-            AST_DECL_DEFINITION_SPLIT(a0, p0, n0, e0);
-            AST_DECL_DEFINITION_SPLIT(a1, p1, n1, e1);
+            auto [p0, n0, e0] = AstDeclDefinition::split(a0);
+            auto [p1, n1, e1] = AstDeclDefinition::split(a1);
             return compare_ast2(n0, n1, e0, e1);
             break;
         }
         case AST_DECL_OPERATOR: {
-            AST_DECL_OPERATOR_SPLIT(a0, p0, c0, e0);
-            AST_DECL_OPERATOR_SPLIT(a1, p1, c1, e1);
+            auto [p0, c0, e0] = AstDeclOperator::split(a0);
+            auto [p1, c1, e1] = AstDeclOperator::split(a1);
             return compare_ast2(c0, c1, e0, e1);
             break;
         }
         case AST_DECL_OBJECT: {
-            AST_DECL_OBJECT_SPLIT(a0, p0, c0, vv0, ff0, ee0);
-            AST_DECL_OBJECT_SPLIT(a1, p1, c1, vv1, ff1, ee1);
+            auto [p0, c0, vv0, ff0, ee0] = AstDeclObject::split(a0);
+            auto [p1, c1, vv1, ff1, ee1] = AstDeclObject::split(a1);
             c = Ast::compare(c0, c1);
             if (c != 0) return c;
             c = compare_asts(vv0, vv1);
@@ -2572,8 +2574,8 @@ int Ast::compare_tag(ast_tag_t t, const AstPtr &a0, const AstPtr &a1) {
             break;
         }
         case AST_DECL_NAMESPACE: {
-            AST_DECL_NAMESPACE_SPLIT(a0, p0, nn0, dd0);
-            AST_DECL_NAMESPACE_SPLIT(a1, p1, nn1, dd1);
+            auto [p0, nn0, dd0] = AstDeclNamespace::split(a0);
+            auto [p1, nn1, dd1] = AstDeclNamespace::split(a1);
             c = compare_texts(nn0, nn1);
             if (c != 0) return c;
             return compare_asts(dd0, dd1);
@@ -2581,14 +2583,14 @@ int Ast::compare_tag(ast_tag_t t, const AstPtr &a0, const AstPtr &a1) {
         }
         // wrapper
         case AST_WRAPPER: {
-            AST_WRAPPER_SPLIT(a0, p0, dd0);
-            AST_WRAPPER_SPLIT(a1, p1, dd1);
+            auto [p0, dd0] = AstWrapper::split(a0);
+            auto [p1, dd1] = AstWrapper::split(a1);
             return compare_asts(dd0, dd1);
             break;
         }
         case AST_DECL_VALUE: {
-            AST_DECL_VALUE_SPLIT(a0, p0, l0, r0);
-            AST_DECL_VALUE_SPLIT(a1, p1, l1, r1);
+            auto [p0, l0, r0] = AstDeclValue::split(a0);
+            auto [p1, l1, r1] = AstDeclValue::split(a1);
             return compare_ast2(l0, l1, r0, r1);
             break;
         }
