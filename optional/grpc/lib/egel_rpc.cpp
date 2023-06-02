a80e9618b84901f4a38f598d06512a2083dc46bb
Marco Devillers <marco.devillers@gmail.com>
2023-06-02 16:55:30 +0200

    prodding at grpc

--- optional/grpc/lib/egel_rpc.cpp
+++ optional/grpc/lib/egel_rpc.cpp
@@ -8,11 +8,15 @@
 
 // #include <src/runtime.hpp> // build against the current distribution
 
-// #include <grpc/grpc.h>
+#include <grpc/grpc.h>
 #include <grpcpp/security/server_credentials.h>
 #include <grpcpp/server.h>
 #include <grpcpp/server_builder.h>
 #include <grpcpp/server_context.h>
+
+#include <egel.pb.h>
+#include <egel.grpc.pb.h>
+
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -21,32 +25,44 @@ using grpc::ServerReader;
 using grpc::ServerReaderWriter;
 using grpc::ServerWriter;
 using grpc::Status;
-using EgelRpc::EgelText;
+using egel_rpc::EgelText;
+using egel_rpc::EgelRpc;
+
 
 #define LIBRARY_VERSION_MAJOR "0"
 #define LIBRARY_VERSION_MINOR "0"
 #define LIBRARY_VERSION_PATCH "1"
 
-class EgelRpcImpl final : public EgelRpc::Service {
+class EgelRpcImpl final : public egel_rpc::EgelRpc::Service {
 public:
     explicit EgelRpcImpl(const std::string& db) {
         egel_rpc::ParseDb(db, &feature_list_);
     }
 
-    Status EgelCall(ServerContext* context, const EgelText* in, EgelText* out) override {
+/*
+    Service();
+    virtual ~Service();
+    virtual ::grpc::Status EgelCall(::grpc::ServerContext* context, const ::egel_rpc::EgelText* request, ::egel_rpc::EgelText* response);
+    virtual ::grpc::Status EgelStream(::grpc::ServerContext* context, ::grpc::ServerReaderWriter< ::egel_rpc::EgelText, ::egel_rpc::EgelText>* stream);
+    virtual ::grpc::Status EgelNodeInfo(::grpc::ServerContext* context, const ::egel_rpc::EgelText* request, ::egel_rpc::EgelText* response);
+*/
+
+    virtual Status EgelCall(ServerContext* context, const EgelText* in, EgelText* out) override {
         auto s = in->text();
         return Status::OK;
     }
 
-    Status EgelStream(ServerContext* context, ServerReaderWriter<EgelText, EgelText>* stream) override {
+    virtual Status EgelStream(ServerContext* context, ServerReaderWriter<EgelText, EgelText>* stream) override {
         EgelText text;
         while (stream->Read(&text)) {
             stream->Write(text); // XXX
         }
+        return Status::OK;
     }
 
-    return Status::OK;
-  }
+    virtual Status EgelNodeInfo(ServerContext* context, const EgelText* in, EgelText* out) override {
+        return Status::OK;
+    }
 
 private:
   std::vector<Feature> feature_list_;
