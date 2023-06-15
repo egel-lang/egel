4ebc63b21b8baa7cebb202ef470068bf0a9a15bc
Marco Devillers <marco.devillers@gmail.com>
2023-06-16 01:01:41 +0200

    final peddles

--- optional/grpc/lib/egel_rpc.cpp
+++ optional/grpc/lib/egel_rpc.cpp
@@ -112,7 +112,8 @@ public:
         return Status::OK;
     }
 
-    void run_server(const std::string& server_address) {
+    void run(VM* m, const std::string& server_address) {
+        set_machine(m);
         ServerBuilder builder;
         builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
         builder.RegisterService(this);
@@ -132,6 +133,9 @@ struct EgelRpcReturn {
 
 class EgelRpcConnection {
 public:
+    EgelRpcConnection() {
+    }
+
     EgelRpcConnection(std::shared_ptr<Channel> channel)
       : _stub(EgelRpc::NewStub(channel)) {}
 
@@ -232,5 +236,154 @@ public:
     }
 
 private:
-    std::unique_ptr<EgelRpc::Stub> _stub;
+    std::unique_ptr<EgelRpc::Stub> _stub = nullptr;
 };
+
+const icu::UnicodeString STRING_SYSTEM = "System";
+
+//## System::rpc_connection - a connection to an egel rpc server
+class RpcConnection : public Opaque {
+public:
+    OPAQUE_PREAMBLE(VM_SUB_EGO, RpcConnection, STRING_SYSTEM, "rpc_connection");
+
+    RpcConnection(VM* m, const std::string& address) : RpcConnection(m) {
+        _address = address;
+        _connection = new EgelRpcConnection(address);
+    }
+
+    ~RpcConnection() {
+        delete _connection;
+    }
+
+    static std::shared_ptr<RpcConnection> cast(const VMObjectPtr& o) {
+        return std::static_pointer_cast<RpcConnection>(o);
+    }
+
+    static VMObjectPtr create(VM* m, const std::string& address) {
+        return VMObjectPtr(new RpcConnection(m, address));
+    }
+
+    int compare(const VMObjectPtr &o) {
+        return -1; // XXX for now
+    }
+
+    VMObjectPtr call(const VMObjectPtr& o) {
+        // send the bundle
+        auto oo = machine()->bundle(o);
+        std::vector<std::string> ss;
+        for (auto &o : oo) {
+            auto s = machine()->disassemble(o);
+            ss.push_back(unicode_to_string(s));
+        }
+        auto r = _connection->EgelBundle(ss);
+
+        if (!r.okay) {
+            throw machine()->create_text("call failed");
+        }
+
+        // do the call
+        auto s = machine()->serialize(o);
+        r = _connection->EgelCall(unicode_to_string(s));
+
+        if (!r.okay) {
+            throw machine()->create_text("call failed");
+        }
+
+        auto q = machine()->deserialize(unicode_from_string(r.text));
+        if (r.exception) {
+            throw q;
+        } else {
+            return q;
+        }
+    }
+
+    VMObjectPtr import_(const icu::UnicodeString& s) {
+        auto r = _connection->EgelImport(unicode_to_string(s));
+
+        if (!r.okay) {
+            throw machine()->create_text("call failed");
+        } else {
+            return machine()->create_none();
+        }
+    }
+
+private:
+    std::string _address;
+    EgelRpcConnection* _connection = nullptr;
+};
+
+class RpcServer : public Monadic {
+public:
+    MONADIC_PREAMBLE(VM_SUB_EGO, RpcServer, STRING_SYSTEM, "rpc_server");
+
+    VMObjectPtr apply(const VMObjectPtr& arg0) const override {
+        if (machine()->is_text(arg0)) {
+            auto s = unicode_to_string(machine()->get_text(arg0));
+            EgelRpcImpl erpc;
+            erpc.run(machine(),s);
+            return machine()->create_none();
+        } else {
+            throw machine()->bad_args(this, arg0);
+        }
+    }
+};
+
+
+class RpcClient: public Monadic {
+public:
+    MONADIC_PREAMBLE(VM_SUB_EGO, RpcClient, STRING_SYSTEM, "rpc_client");
+
+    VMObjectPtr apply(const VMObjectPtr& arg0) const override {
+        if (machine()->is_text(arg0)) {
+            auto s = unicode_to_string(machine()->get_text(arg0));
+            return RpcConnection::create(machine(), s);
+        } else {
+            throw machine()->bad_args(this, arg0);
+        }
+    }
+};
+
+class RpcCall: public Dyadic {
+public:
+    DYADIC_PREAMBLE(VM_SUB_EGO, RpcCall, STRING_SYSTEM, "rpc_call");
+
+    VMObjectPtr apply(const VMObjectPtr& arg0, const VMObjectPtr& arg1) const override {
+        auto m = machine();
+        if (m->is_opaque(arg0) && m->symbol(arg0) == "System::rpc_connection") {
+            auto c = RpcConnection::cast(arg0);
+            return c->call(arg1);
+        } else {
+            throw machine()->bad_args(this, arg0, arg1);
+        }
+    }
+};
+
+class RpcImport: public Dyadic {
+public:
+    DYADIC_PREAMBLE(VM_SUB_EGO, RpcImport, STRING_SYSTEM, "rpc_import");
+
+    VMObjectPtr apply(const VMObjectPtr& arg0, const VMObjectPtr& arg1) const override {
+        auto m = machine();
+        if (m->is_opaque(arg0) && m->symbol(arg0) == "System::rpc_connection" && m->is_text(arg1)) {
+            auto c = RpcConnection::cast(arg0);
+            return c->import_(m->get_text(arg1));
+        } else {
+            throw machine()->bad_args(this, arg0, arg1);
+        }
+    }
+};
+
+extern "C" std::vector<icu::UnicodeString> egel_imports() {
+    return std::vector<icu::UnicodeString>();
+}
+
+extern "C" std::vector<VMObjectPtr> egel_exports(VM* vm) {
+    std::vector<VMObjectPtr> oo;
+
+    oo.push_back(RpcConnection::create());
+    oo.push_back(RpcServer::create(vm));
+    oo.push_back(RpcCall::create(vm));
+    oo.push_back(RpcImport::create(vm));
+
+    return oo;
+}
