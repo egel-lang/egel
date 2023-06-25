afee09ebf9cef6fbe5b423d885c167913dfd5c29
Marco Devillers <marco.devillers@gmail.com>
2023-06-25 21:13:50 +0200

    revert

--- src/operators.hpp
+++ src/operators.hpp
@@ -48,9 +48,9 @@ static operator_t operators[] = {
     {'%', "ct", RIGHT_ASSOC | INFIX},
     {'~', "tl", NONE_ASSOC | PREFIX | INFIX},
     {'@', "at", LEFT_ASSOC | INFIX},
-    {'&', "am", RIGHT_ASSOC | INFIX},
+    {'&', "am", LEFT_ASSOC | INFIX},
+    {'$', "or", LEFT_ASSOC | INFIX},
     {'|', "br", LEFT_ASSOC | INFIX},
-    {'$', "do", RIGHT_ASSOC | INFIX},
     {'.', "dt", LEFT_ASSOC | INFIX},
 };
 
