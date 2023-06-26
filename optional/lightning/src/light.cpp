d180fc3d05699318479b03702231e8d771d70677
Marco Devillers <marco.devillers@gmail.com>
2023-06-26 15:35:09 +0200

    work, work

--- optional/lightning/src/light.cpp
+++ optional/lightning/src/light.cpp
@@ -1,11 +1,161 @@
-#include "instructions.hpp"
-
 #include <egel/runtime.hpp>
 #include <egel/bytecode.hpp>
 
 #include <lightning.h>
 
 using namespace egel;
+
+#define TRACE_JIT(x)    x;
+
+extern "C" {
+
+using namespace egel;
+
+const int VM_OBJECT_PTR_SIZE = sizeof(VMObjectPtr);
+
+// create registers
+inline VMObjectPtr* alloc_vm_object_ptr() {
+    return static_cast<VMObjectPtr*>(malloc(VM_OBJECT_PTR_SIZE));
+};
+
+inline void vm_object_ptr_construct(VMObjectPtr* p) {
+    new (p) VMObjectPtr();
+};
+
+inline void vm_object_ptr_construct_n(VMObjectPtr* p, int n) {
+    for (int i = 0; i < n; i++) {
+        new (&(p[i])) VMObjectPtr();
+    }
+};
+
+inline void vm_object_ptr_destruct(VMObjectPtr* p) {
+    p->~VMObjectPtr();
+};
+
+inline void vm_object_ptr_destruct_n(VMObjectPtr* p, int n) {
+    for (int i = 0; i < n; i++) {
+        p[i].~VMObjectPtr();
+    }
+};
+
+inline void vm_object_ptr_assign(VMObjectPtr* p0, VMObjectPtr *p1) {
+    *p0 = *p1;
+};
+
+// OP_NIL x,  x := null
+inline void op_nil(VM *vm, VMObjectPtr *a, int x) {
+    TRACE_JIT(std::cerr << "OP_NIL " << x << std::endl);
+    a[x] = nullptr;
+};
+
+// OP_MOV x y, x := y
+inline void op_mov(VM *vm, VMObjectPtr *a, int x, int y) {
+    TRACE_JIT(std::cerr << "OP_MOV " << x << ", " << y << std::endl);
+    a[x] = a[y];
+};
+
+// OP_DATA x i32, x := data(i32)
+inline void op_data(VM* vm, VMObjectPtr *a, int x, int i) {
+    a[x] = vm->get_data(i);
+};
+
+// OP_ARRAY x y z, x := [ y, y+1,.., z ]
+inline void op_array(VM* vm, VMObjectPtr *a, int x, int y, int z) {
+    TRACE_JIT(std::cerr << "OP_ARRAY " << x << ", " << y << ", " << z << std::endl);
+    auto n = (z - y) + 1;
+    auto a0 = VMObjectArray::create(n);
+    auto a1 = VMObjectArray::cast(a0);
+    for (int i = 0; i < n; i++) {
+        a1->set(i, a[y+i]);
+    }
+    a[x] = a1;
+};
+
+// OP_SET x y z, x[val(y)] = z
+inline void op_set(VM* vm, VMObjectPtr* a, int x, int  y, int z) {
+    TRACE_JIT(std::cerr << "OP_MOV " << x << ", " << y << ", " << z << std::endl);
+    int n = VMObjectInteger::cast(a[y])->value();
+    auto a0 = VMObjectArray::cast(a[x]);
+    a0->set(n, a[z]);
+};
+
+// OP_TAKEX x y z i16, x,..,y = z[i],..,z[i+y-x], ~flag if fail
+inline void op_takex(VM* vm, VMObjectPtr* a, int x, int y, int z, int n1, bool *flag) {
+    TRACE_JIT(std::cerr << "OP_TAKEX " << x << ", " << y << ", " << z << ", " << n1 << std::endl);
+    int n0 = (y - x) + 1;
+    auto a0 = VMObjectArray::cast(a[z]);
+    if (a0->size() < n0 + n1) {
+        *flag = false;
+    } else {
+        for (int i = 0; i < n0; i++) {
+            a[x+i] = a0->get(n1+i);
+        }
+    }
+};
+
+// OP_SPLIT x y z, x,..,y = z[0],..,z[y-x], flag if not exact
+inline void op_split(VM* vm, VMObjectPtr* a, int x, int y, int z, int* flag) {
+    TRACE_JIT(std::cerr << "OP_SPLIT " << x << ", " << y << ", " << z << std::endl);
+    auto n = (y - x) + 1;
+    auto a0 = VMObjectArray::cast(a[z]);
+    if (a0->size() != n) {
+        *flag = (int) false;
+    } else {
+        *flag = (int) true;
+        for (int i = 0; i < n; i++) {
+            a[x+i] = a0->get(i);
+        }
+    }
+};
+
+// OP_CONCATX x y z i16, x := y ++ drop i z
+inline void op_concatx(VM* vm, VMObjectPtr *a, int x, int y, int z, int n) {
+    TRACE_JIT(std::cerr << "OP_CONCATX " << x << ", " << y << ", " << z << ", " << n << std::endl);
+    auto y0 = VMObjectArray::cast(a[y]);
+    auto z0 = VMObjectArray::cast(a[z]);
+
+    int n0 = y0->size();
+    int n1 = z0->size();
+
+    if (n1 <= n) {
+        a[x] = a[y];
+    } else {
+        auto q0 = VMObjectArray::create(n0+n1-n);
+        auto q1 = VMObjectArray::cast(q0);
+        for (int i = 0; i < n0; i++) {
+            q1->set(i, y0->get(i));
+        }
+        for (int i = n0; i < n0+n1-n; i++) {
+            q1->set(i, z0->get(i+n-n0));
+        }
+        a[x] = q1; 
+    }
+};
+
+// OP_TEST x y, flag := (x == y)
+inline void op_test(VM* vm, VMObjectPtr *a, int x, int y, int* flag) {
+    TRACE_JIT(std::cerr << "OP_CONCATX " << x << ", " << y << std::endl);
+    EqualVMObjectPtr equals;
+    bool b = equals(a[x], a[y]);
+    *flag = (int) b;
+};
+
+// OP_TAG x y, flag := (x->tag() == y)
+inline void op_tag(VM* vm, VMObjectPtr *a, int x, int y, bool* flag) {
+    TRACE_JIT(std::cerr << "OP_TAG " << x << ", " << y << std::endl);
+    auto s0 = (a[x])->symbol(); // XXX, wut?
+    auto s1 = (a[y])->symbol();
+    bool b = ( s0 == s1 );
+    *flag = b;
+};
+
+// OP_RETURN x
+inline void op_return(VM*, VMObjectPtr *a, int x, VMObjectPtr *ret) {
+    TRACE_JIT(std::cerr << "OP_RETURN " << x << std::endl);
+    *ret = a[x];
+};
+
+}; // extern "C"
 
 class BytecodePass {
 public:
@@ -509,7 +659,7 @@ public:
 
         // reserve space in the stack for registers and flag
         auto regs_offset = jit_allocai(regs * sizeof(VMObjectPtr));
-        auto flag_offset = jit_allocai(sizeof(bool));
+        auto flag_offset = jit_allocai(sizeof(int));
 
 
         // R1 = registers
